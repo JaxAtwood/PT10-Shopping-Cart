8ec6a094d2331d58bf255576d8b237425d220a72
Jackie Atwood <takalaj@verizon.net>
2019-12-19 21:56:31 -0500

    MVP met, attempting stretch

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 function App() {
 	const [products] = useState(data);
@@ -23,15 +24,15 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
+		<CartContext.Provider value={{ cart }}>
 		<div className="App">
 
-			<Navigation cart={cart} />
+			<Navigation />
 			<Route exact path="/" component={Products} />
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+			<Route path="/cart" component={ShoppingCart} />
 			/>
 		</div>
+		</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
